9ab0a6f1be0a8010a5fae0f895ca68755504fa85
dm <muk_dinara@mail.ru>
2019-02-19 16:58:17 +0600

    Добавьте файлы проекта.

---
+++ TankConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tanks.WordOfTanks;
+
+namespace TankConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<Tank> t34 = new List<Tank>();
+            List<Tank> panters = new List<Tank>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int x = rnd.Next(100), y = rnd.Next(100), z = rnd.Next(100);
+                Tank t = new Tank("T-34", x, y, z);
+                t34.Add(t);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                int x = rnd.Next(100), y = rnd.Next(100), z = rnd.Next(100);
+                Tank t = new Tank("Pantera", x, y, z);
+                panters.Add(t);
+            }
+                
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (t34[i] * panters[i] == true)
+                    Console.WriteLine("Победил T-34: {0} vs {1}", t34[i].ToString(), panters[i].ToString());
+                else
+                    Console.WriteLine("Победил Pantera: {1} vs {0}", t34[i].ToString(), panters[i].ToString());
+            }
+        }
+    }
+}
---
+++ Tanks/WordOfTanks/Tank.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanks.WordOfTanks
+{
+    //public static Random rnd = new Random();
+    public class Tank
+    {
+        Random rnd = new Random();
+        public string Name { get; set; }
+        private int Ammunition;
+        private int Armor;
+        private int Mobility;
+
+        public Tank()
+        {
+            Ammunition = rnd.Next(100);
+            Armor = rnd.Next(100);
+            Mobility = rnd.Next(100);
+        }
+        public Tank(string name)
+        {
+            this.Name = name;
+            this.Ammunition = rnd.Next(100);
+            this.Armor = rnd.Next(100);
+            this.Mobility = rnd.Next(100);           
+        }
+        public Tank(string name, int ammunition, int armor, int mobility)
+        {
+            this.Name = name;
+            this.Ammunition = ammunition;
+            this.Armor = armor;
+            this.Mobility = mobility;
+        }
+        public static bool operator * (Tank a, Tank b)
+        {
+            return (a.Ammunition > b.Ammunition && a.Armor > b.Armor) || (a.Ammunition>b.Ammunition && a.Mobility>b.Mobility)
+                || (a.Armor>b.Armor && a.Mobility>b.Mobility); 
+        }
+
+        public override string ToString()
+        {
+            return (Name + ";" + Ammunition.ToString() + ";" + Armor.ToString() + ";" + Mobility.ToString());
+        }
+
+    }
+}
